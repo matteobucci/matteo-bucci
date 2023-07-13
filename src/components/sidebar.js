0623051a2b13956d5daaa4e2f68cf46dcd57212e
Matteo Bucci <matteo.bucci.1995@gmail.com>
2023-07-14 00:04:26 +0200

    Added some gifs just for fun on stats page

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import me from "../images/matteo-2021.jpg";
+import { StaticImage } from "gatsby-plugin-image";
 
 export const AboutSidebar = () => {
   return (
     <aside className="post-sidebar">
       <div className="post-sidebar-card">
         <h2>Me</h2>
-        <img src={me} alt="Matteo Bucci" />
+        <StaticImage
+          alt="Matteo Bucci"
+          src={me}
+        />
       </div>
     </aside>
   );
--- src/pages/stats.js
+++ src/pages/stats.js
@@ -2,19 +2,25 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import skiingGif from '../images/skiing.gif'
+import runningGif from '../images/running.gif'
+import motorbikeGif from '../images/motorbiking.gif'
 
 const Statistics = () => {
   return (
     <div>
-      <div class="card">
+      <div class="card stats-card">
+        <img class="stats-icon" alt="" src={skiingGif} />
         <div class="stat-title">Skiing done so far</div>
         <div class="stat-value">453km</div>
       </div>
-      <div class="card">
+      <div class="card stats-card">
+        <img class="stats-icon" alt="" src={runningGif} />
         <div class="stat-title">Total running so far</div>
         <div class="stat-value">1534km</div>
       </div>
-      <div class="card">
+      <div class="card stats-card">
+        <img class="stats-icon" alt="" src={motorbikeGif} />
         <div class="stat-title">Motorbike trips so far</div>
         <div class="stat-value">26023km</div>
       </div>
